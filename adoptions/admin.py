73a3061673b36026ea7c66d2f45912f6305e3548
Natalya Pimankova <natalya2487@gmail.com>
2020-05-02 21:23:05 +0300

    create app adoptions, models

---
+++ adoptions/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Pet, Vaccine
+
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ['name', 'species', 'breed', 'age', 'sex']
+
+
+@admin.register(Vaccine)
+class VaccineAdmin(admin.ModelAdmin):
+    list_display = ['name', ]
